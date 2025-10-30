4ade592af42582757f5fc6549c02c93917d2a4a9
harold-enciso <harold.enciso.v@uni.pe>
2025-10-30 03:49:03 -0500

    Adaptacion 1

--- frontend/src/pages/games/DominoFracciones.jsx
+++ frontend/src/pages/games/DominoFracciones.jsx
@@ -1,8 +1,18 @@
 import "./Juegos.css";
-
+import { useState } from "react";
 import HeaderPrincipal from "../../components/HeaderPrincipal";
 export default function DominoFracciones(){
 
+    //Fichas iniciales de prueba front
+    const [fichas,setFichas] = useState([
+        {id: 1, fracciones: ["1/2","1/4"]},
+        {id: 2, fracciones: ["2/8","1/3"]},
+        {id: 3, fracciones: ["3/9","1/6"]}
+    ])
+    //Espacios
+    const [espacios,setEspacios] = useState([null,null,null])
+
+    
     return(
         <>
         <HeaderPrincipal />
