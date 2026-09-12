26082ce8049ef69d69778c0fa165f94cb9eca8e4
harold-enciso <harold.enciso.v@uni.pe>
2026-09-12 01:04:06 -0500

    Ajustes para Cloudflare R2

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,14 +10,33 @@ export default function Register() {
     const {showToast} = useToast();
     const [contrasena,setContrasena] = useState("");
     const [contrasenaTocada,setContrasenaTocada] = useState(false);
+    const [contrasena2,setContrasena2] = useState("");
+    const [contrasena2Tocada,setContrasena2Tocada] = useState(false);
     const [correo,setCorreo] = useState("");
     const [correoTocado,setCorreoTocado] = useState(false);
     const apiUrl = import.meta.env.VITE_API_URL;
     const registerUrl = `${apiUrl}/auth/register`
     //POST
     const handleRegister = () => {
-        if (!correo || !contrasena) {
+        if (!correo || !contrasena || !contrasena2) {
             showToast("Completa todos los campos","warning");
+            
+            return;
+        }
+        if (!correo.includes("@")) {
+            showToast("Ingresa un correo válido","warning");
+            return;
+        }
+        if (!correo.includes(".")) {
+            showToast("Ingresa un correo válido","warning");
+            return;
+        }
+        if (correo.split("@").length !==  2) {
+            showToast("Ingresa un correo válido","warning");
+            return;
+        }
+        if (correo.endsWith(".")) {
+            showToast("Ingresa un correo válido","warning");
             return;
         }
         showLoading("Registrando tu correo...");
@@ -46,8 +65,12 @@ export default function Register() {
             navigate("/login");
         })
         .catch(err =>{
-            console.log(err.detail)
+            console.log("Err.detail: " + err.detail)
             hideLoading();
+            if(err.detail === "Email ya registrado") {
+                showToast("Email ya registrado","error");
+                return;
+            }
             showToast("Error, email incorrecto o repetido","error");
             
             setCorreo("");
@@ -82,6 +105,17 @@ export default function Register() {
                 onBlur={() => setContrasenaTocada(true)}
                 />
                 <span className={contrasenaTocada && contrasena.length===0 ? "error-visible":"error-oculto"}>
+                    Ingrese su contraseña</span>
+
+                <h2>Confirme su contraseña</h2>
+                <input
+                type="password"
+                placeholder=""
+                value={contrasena2}
+                onChange={(e) => setContrasena2(e.target.value)}
+                onBlur={() => setContrasena2Tocada(true)}
+                />
+                <span className={contrasena2Tocada && contrasena2.length===0 ? "error-visible":"error-oculto"}>
                     Ingrese su contraseña</span>
                 <button className="boton-login" onClick={handleRegister}>
                     Regístrate
