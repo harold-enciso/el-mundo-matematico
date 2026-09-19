4c2f4f167fe46444233b7159848b936de89d0db8
harold-enciso <harold.enciso.v@uni.pe>
2026-09-19 06:29:47 -0500

    Security Headers Back

--- frontend/src/pages/games/Pentomino.jsx
+++ frontend/src/pages/games/Pentomino.jsx
@@ -50,7 +50,7 @@ export default function Pentomino(){
                     )}
                     {pdfViewerUrl && (
                         <iframe
-                            src={pdfViewerUrl}
+                            src={pdfUrl}
                             width="100%"
                             height="100%" 
                             title="PDF"
